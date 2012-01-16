dddc3286adb238bc81bc0a77701f44d4ecad70d0
Jason Fung <jasonfungsing@gmail.com>
2012-01-16 23:00:29 +1100

    Initialize the project

---
+++ AndroidServices/src/com/jasonfc/AndroidServicesActivity.java
@@ -0,0 +1,13 @@
+package com.jasonfc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidServicesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
